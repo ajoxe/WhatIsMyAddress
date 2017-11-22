2d1e651854188b564f70fbe8709ae196c3432cda
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2017-11-21 21:58:52 -0500

    part03 unfinished commit

--- app/src/main/java/nyc/c4q/whatismyaddress/DisplayActivity.java
+++ app/src/main/java/nyc/c4q/whatismyaddress/DisplayActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DisplayActivity extends AppCompatActivity {
-
+Bundle displayBundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        displayBundle = getIntent().getExtras();
     }
 }
